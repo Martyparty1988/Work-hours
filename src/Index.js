8c4f8c987bd9030710bc15f5ceb91a68cd462e2b
Marty <rosary-vassals8m@icloud.com>
2025-04-07 18:36:36 +0200

    Create Index.js

---
+++ src/Index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
